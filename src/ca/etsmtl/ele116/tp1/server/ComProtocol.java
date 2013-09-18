ba1eb25fd7f0119b876f2295b9eb9c7815342362
Gregory A. Lussier <greg@gregoryalussier.com>
2013-09-18 13:53:10 -0400

    Changed coordinates values: all shapes are now started in the first horizontal half of the window.

--- src/ca/etsmtl/ele116/tp1/server/ComProtocol.java
+++ src/ca/etsmtl/ele116/tp1/server/ComProtocol.java
@@ -32,35 +32,35 @@ public class ComProtocol {
 			switch (r.nextInt(5)) {
 				case 0: // CARRE
 					shape = "CARRE";
-					params[0] = r.nextInt(400);
-					params[1] = r.nextInt(400);
+					params[0] = r.nextInt(250);
+					params[1] = r.nextInt(250);
 					params[2] = (int) (params[0] + r.nextFloat() * (500 - params[0]));
 					params[3] = params[1] + (params[2] - params[0]);
 					break;
 				case 1: // RECTANGLE
 					shape = "RECTANGLE";
-					params[0] = r.nextInt(400);
-					params[1] = r.nextInt(400);
+					params[0] = r.nextInt(250);
+					params[1] = r.nextInt(250);
 					params[2] = (int) (params[0] + r.nextFloat() * (500 - params[0]));
 					params[3] = (int) (params[1] + r.nextFloat() * (500 - params[1]));
 					break;
 				case 2: // CERCLE
 					shape = "CERCLE";
-					params[0] = 100 + r.nextInt(300);
-					params[1] = 100 + r.nextInt(300);
-					params[2] = r.nextInt(100);
+					params[0] = r.nextInt(250);
+					params[1] = r.nextInt(250);
+					params[2] = r.nextInt(400);
 					break;
 				case 3: // ELLIPSE
 					shape = "ELLIPSE";
-					params[0] = 100 + r.nextInt(300);
-					params[1] = 100 + r.nextInt(300);
-					params[2] = r.nextInt(100);
-					params[3] = r.nextInt(100);
+					params[0] = r.nextInt(250);
+					params[1] = r.nextInt(250);
+					params[2] = r.nextInt(400);
+					params[3] = r.nextInt(400);
 					break;
 				case 4: // LIGNE
 					shape = "LIGNE";
-					params[0] = r.nextInt(400);
-					params[1] = r.nextInt(400);
+					params[0] = r.nextInt(250);
+					params[1] = r.nextInt(250);
 					params[2] = params[0] + r.nextInt(500-params[0]);
 					params[3] = params[1] + r.nextInt(500-params[1]);
 					break;
